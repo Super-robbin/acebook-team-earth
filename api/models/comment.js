db932130433ba97afb4643ab0230d49eb340795a
Roberto <roberto.quadraccia90@gmail.com>
2023-09-22 09:46:58 +0200

    Models updated with new schemas

--- api/models/comment.js
+++ api/models/comment.js
@@ -1,8 +1,14 @@
 const mongoose = require("mongoose");
 
 const CommentSchema = new mongoose.Schema({
-    comment: String
+    content: { type: String, required: true },
+    user: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    }]
 });
 
 const Comment = mongoose.model("Comment", CommentSchema);
+const doc = new Comment();
+console.log(CommentSchema)
 module.exports = Comment;
