6db33528d04e1e27fae6e22d70042157f368653e
Roberto <roberto.quadraccia90@gmail.com>
2023-09-28 11:58:56 +0200

    Space

--- frontend/src/components/comment/Comment.js
+++ frontend/src/components/comment/Comment.js
@@ -1,6 +1,6 @@
 import React from "react";
 import defaultProfilePic from "../../images/default_profile_pic.jpg";
-import '../../styles/card/card.css';
+import "../../styles/card/card.css";
 
 const Comment = ({ comment }) => {
   const commentedAt = new Date(comment.createdAt);
@@ -12,26 +12,28 @@ const Comment = ({ comment }) => {
     minute: "2-digit",
   });
 
-    return (
-        <>
-            <div className="comment__section" key={ comment._id }>
-                <div className="card__container">
-                    <img
-                        className="card__img comment_img"
-                        alt="user pic"
-                        src={
-                            comment.user.picture ? comment.user.picture : defaultProfilePic
-                          }
-                        />
-                    <div className="card__user-info">
-                        <p className="card__text_name comment_text">{comment.user.username}</p>
-                        <p className="card__text_time" >{commentedAtFormatted}</p>
-                    </div>
-                </div>
-                <p className="comment__content">{comment.content}</p>
-            </div>
-        </>
-    )
-}
+  return (
+    <>
+      <div className="comment__section" key={comment._id}>
+        <div className="card__container">
+          <img
+            className="card__img comment_img"
+            alt="user pic"
+            src={
+              comment.user.picture ? comment.user.picture : defaultProfilePic
+            }
+          />
+          <div className="card__user-info">
+            <p className="card__text_name comment_text">
+              {comment.user.username}
+            </p>
+            <p className="card__text_time">{commentedAtFormatted}</p>
+          </div>
+        </div>
+        <p className="comment__content">{comment.content}</p>
+      </div>
+    </>
+  );
+};
 
 export default Comment;
