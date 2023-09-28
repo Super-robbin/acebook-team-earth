d9e5330974d0e533859c6fedcdee91bc766dbf34
alalina <alalinaerm@gmail.com>
2023-09-28 14:51:42 +0100

    added default profile pic to FeedHeader

--- frontend/src/components/feed_header/FeedHeader.js
+++ frontend/src/components/feed_header/FeedHeader.js
@@ -2,6 +2,7 @@ import React from 'react';
 import '../../styles/buttons/buttons.css';
 import '../../styles/container/container.css';
 import logout_icon from '../../images/logout.svg';
+import defaultProfilePic from "../../images/default_profile_pic.jpg";
 
 const FeedHeader = ({ user, logout }) => {
     return (
@@ -9,7 +10,7 @@ const FeedHeader = ({ user, logout }) => {
             <header className="header__feed">
                 <h1>ACEBOOK</h1>
                 <div className="user__info">
-                    <img className="user__img" src={user.picture} alt="user pic" />
+                    <img className="user__img" src={user.picture ? user.picture : defaultProfilePic} alt="user pic" />
                     <p>{user.username}</p>
                     <button className="button__log-out" onClick={logout}>
                         <img className="sign" src={logout_icon} alt="logout-icon" />
